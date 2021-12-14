6a9695d2eab742624b8c6d3179c148c30d9a850f
Daniel <dkilcullen25@gmail.com>
2021-12-13 21:59:59 -0600

    commit tree 3216d2fe647c2e809da1726830fdd0ea480bfbae

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Write your solution in this file
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'jeff'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not jeff'
+} 
